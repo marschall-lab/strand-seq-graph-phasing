f834f56150e698f08e7b3c09b1491d2443a6090f
Mir Henglin <mir.henglin@gmail.com>
2022-10-18 20:02:38 +0200

    Cleanup simplify_gfa.py

--- wd/scripts/python/simplify_gfa.py
+++ wd/scripts/python/simplify_gfa.py
@@ -35,40 +35,32 @@ if thresh is None and clust_file is None:
 # Import ------------------------------------------------------------------
 
 graph = gfapy.Gfa.from_file(args.input[0], vlevel = 3)
-# graph = gfapy.Gfa.from_file('HG00211/assembly.homopolymer-compressed_graph_components/simplified_assembly.gfa', vlevel = 3)
-# graph = gfapy.Gfa.from_file('../input/HG002/assembly/assembly.homopolymer-compressed.gfa', vlevel = 3)
-# thresh=1000000
 
 def simplify_graph(graph, thresh=None, segments_to_keep_names=None):
     # remove self links is probably called more than necessary, but it fixed bugs.
     
     simplified_graph = remove_self_links(graph)
     simplified_graph = remove_solo_segments(simplified_graph, thresh, segments_to_keep_names)
-    simplified_graph.to_file('test_all0.gfa')
     
     print('macro simplify')
     simplified_graph = remove_self_links(simplified_graph)
     simplified_graph = macro_simplify(simplified_graph, thresh, segments_to_keep_names)
     simplified_graph = remove_self_links(simplified_graph)
     simplified_graph = remove_solo_segments(simplified_graph, thresh, segments_to_keep_names)
-    simplified_graph.to_file('test_all1.gfa')
     
     print('fine simplify')
     simplified_graph = remove_self_links(simplified_graph)
     simplified_graph = remove_nodes(simplified_graph, thresh, segments_to_keep_names) 
-    simplified_graph.to_file('test_all2.gfa')
     
     print('cleanup and crimp')
     simplified_graph = remove_self_links(simplified_graph)
     simplified_graph = remove_dead_ends(simplified_graph, thresh, segments_to_keep_names)
-    simplified_graph.to_file('test_all3.gfa')
     
     return simplified_graph
     
 def simplify_and_crimp_graph(graph, thresh=None, segments_to_keep_names=None):
     simplified_graph= simplify_graph(graph, thresh, segments_to_keep_names)
     simplified_graph = crimp(simplified_graph)
-    simplified_graph.to_file('test_all4.gfa')
     
     return simplified_graph
 
@@ -80,7 +72,6 @@ if not graph.segments and not graph.dovetails:
 elif args.clusters is not None:
     # TODO
     clusts_file = args.clusters[0]
-    # clusts_file =  '../HG002/SaaRclust/Clusters/MLclust.data'
 
     clust_rname = dict()
     with open(clusts_file, 'r') as file:
@@ -100,21 +91,18 @@ elif args.clusters is not None:
     all_clustered_rnames = []
     for x in clust_rname.values():
         all_clustered_rnames.extend(x)
-    # Speed things up by doing an oll over simpmlify before, which reduces a 
+    # Speed things up by doing an all over simplify before, which reduces a 
     # lot of reduncant work when simplifying each cluster 
     simplified_graph = simplify_graph(graph, thresh=thresh, segments_to_keep_names=all_clustered_rnames) 
-    # segments_to_keep_names = keep_names = clust_rname['17']
     
-    subsampled_dict = random.sample(list(clust_rname), 2)
-    subsampled_dict = {k : clust_rname[k] for k in subsampled_dict}
     simplified_graphs = [simplify_and_crimp_graph(simplified_graph, thresh=thresh, segments_to_keep_names=keep_names) for keep_names in clust_rname.values()]
     simplified_graph = graph_from_graph_list(simplified_graphs, overlap=False)
 else:
     simplified_graph = simplify_and_crimp_graph(graph, thresh=thresh)
    
 print('export')
-# TODO
+
 simplified_graph.to_file(args.output[0])
-# simplified_graph.to_file('test_graph3.gfa')
+
     
   
